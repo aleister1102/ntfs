6579ed0dfa4ee3a259fcebae8f6c93d48903f914
Lê Minh Quân <marucube35@gmail.com>
2022-10-05 20:00:35 +0700

    refactor

--- src/ReadSector.cpp
+++ src/ReadSector.cpp
@@ -142,24 +142,6 @@ SYSTEMTIME convertFileTimeToDateTime(uint64_t filetime)
     return sys;
 }
 
-uint16_t swap_uint16(uint16_t val)
-{
-    return (val << 8) | (val >> 8);
-}
-
-uint32_t swap_uint32(uint32_t val)
-{
-    val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF);
-    return (val << 16) | (val >> 16);
-}
-
-uint64_t swap_uint64(uint64_t val)
-{
-    val = ((val << 8) & 0xFF00FF00FF00FF00ULL) | ((val >> 8) & 0x00FF00FF00FF00FFULL);
-    val = ((val << 16) & 0xFFFF0000FFFF0000ULL) | ((val >> 16) & 0x0000FFFF0000FFFFULL);
-    return (val << 32) | (val >> 32);
-}
-
 struct FileName
 {
     uint64_t fileReferenceToParentDir;
